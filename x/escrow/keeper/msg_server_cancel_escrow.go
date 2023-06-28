cefc7283d750b3a6f14239e79f5424bbab78f956
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2023-06-28 17:34:11 -0400

    Added logic to Cancel-Escrow message
    - Add logic to cancel an escrow
    - Add errors to the module

--- x/escrow/keeper/msg_server_cancel_escrow.go
+++ x/escrow/keeper/msg_server_cancel_escrow.go
@@ -2,16 +2,39 @@ package keeper
 
 import (
 	"context"
-
 	"dredd-secure/x/escrow/types"
+
+	"cosmossdk.io/errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CancelEscrow(goCtx context.Context, msg *types.MsgCancelEscrow) (*types.MsgCancelEscrowResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	escrow, found := k.GetEscrow(ctx, msg.Id)
 
-	// TODO: Handling the message
-	_ = ctx
+	if !found {
+		return nil, errors.Wrapf(sdkerrors.ErrKeyNotFound, "The escrow with key %d doesn't exist", msg.Id)
+	}
 
+	if escrow.Initiator != msg.Creator {
+		return nil, errors.Wrap(sdkerrors.ErrUnauthorized, "Cannot cancel: not from the initiator")
+	}
+
+	if escrow.Status != "open" {
+		return nil, errors.Wrapf(types.ErrWrongEscrowStatus, "%v", escrow.Status)
+	}
+
+	initiator, _ := sdk.AccAddressFromBech32(escrow.Initiator)
+	collateral := escrow.InitiatorCoins
+	err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, initiator, collateral)
+
+	if err != nil {
+		return nil, err
+	}
+
+	escrow.Status = "cancelled"
+	k.SetEscrow(ctx, escrow)
 	return &types.MsgCancelEscrowResponse{}, nil
 }
--- x/escrow/types/errors.go
+++ x/escrow/types/errors.go
@@ -3,10 +3,11 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"cosmossdk.io/errors"
 )
 
 // x/escrow module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrSample            = errors.Register(ModuleName, 1100, "sample error")
+	ErrWrongEscrowStatus = errors.Register(ModuleName, 2, "Wrong escrow status")
 )
