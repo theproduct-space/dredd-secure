6ebb8692e4e5caf3e5277c8439852759ff0bb628
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2023-07-03 13:23:38 -0400

    init frontend

--- x/escrow/types/message_cancel_escrow_test.go
+++ x/escrow/types/message_cancel_escrow_test.go
@@ -1,28 +1,61 @@
-package types
+package types_test
 
 import (
 	"testing"
 
 	"dredd-secure/testutil/sample"
+	"dredd-secure/x/escrow"
+	"dredd-secure/x/escrow/keeper"
+	"dredd-secure/x/escrow/types"
+
+	keepertest "dredd-secure/testutil/keeper"
+
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestMsgCancelEscrow_ValidateBasic(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		EscrowList: []types.Escrow{
+			{
+				Initiator: "1",
+				Id:        0,
+			},
+			{
+				Initiator: "2",
+				Id:        1,
+			},
+		},
+		EscrowCount: 2,
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.EscrowKeeper(t)
+	escrow.InitGenesis(ctx, *k, genesisState)
+	msgServer := keeper.NewMsgServerImpl(*k)
+	_, err := msgServer.CancelEscrow(ctx, &types.MsgCancelEscrow{
+		Creator: "1",
+		Id:      0,
+	})
+	require.NoError(t, err)
+
 	tests := []struct {
 		name string
-		msg  MsgCancelEscrow
+		msg  types.MsgCancelEscrow
 		err  error
 	}{
 		{
 			name: "invalid address",
-			msg: MsgCancelEscrow{
+			msg: types.MsgCancelEscrow{
 				Creator: "invalid_address",
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
 			name: "valid address",
-			msg: MsgCancelEscrow{
+			msg: types.MsgCancelEscrow{
 				Creator: sample.AccAddress(),
 			},
 		},
