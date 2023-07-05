67175b46cc8d52a0cabd4a545d883feafe06c2d0
jayouimet <jayouimet@hotmail.com>
2023-07-05 09:20:39 -0400

    finished unit tests for query escrows by address

--- x/escrow/keeper/query_escrows_by_address_test.go
+++ x/escrow/keeper/query_escrows_by_address_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	//"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,8 +18,6 @@ import (
 func TestEscrowQueryByAddress(t *testing.T) {
 	keeper, ctx := keepertest.EscrowKeeper(t)
 	wctx := sdk.WrapSDKContext(ctx)
-
-	// msgs := createNEscrow(keeper, ctx, 2)
 
 	addresses := [3]string{"alice", "bob", ""}
 	escrowsResults := [3]int{0, 0, 0}
@@ -81,13 +79,36 @@ func TestEscrowQueryByAddress(t *testing.T) {
 	}
 }
 
-/*func TestEscrowQueryByAddressPaginated(t *testing.T) {
+func TestEscrowQueryByAddressPaginated(t *testing.T) {
 	keeper, ctx := keepertest.EscrowKeeper(t)
 	wctx := sdk.WrapSDKContext(ctx)
-	msgs := createNEscrow(keeper, ctx, 5)
+	// msgs := createNEscrow(keeper, ctx, 5)
 
-	request := func(next []byte, offset, limit uint64, total bool) *types.QueryAllEscrowRequest {
-		return &types.QueryAllEscrowRequest{
+	addresses := [3]string{"alice", "bob", ""}
+	escrowsResults := [3]int{0, 0, 0}
+
+	items := make([]types.Escrow, 20)
+	var items_test []types.Escrow
+    for i := range items {
+		r1 := rand.Intn(3)
+		items[i].Initiator = addresses[r1]
+		r2 := rand.Intn(3)
+		items[i].Fulfiller = addresses[r2]
+        items[i].Id = keeper.AppendEscrow(ctx, items[i])
+
+		escrowsResults[r1] += 1
+		if r1 != r2 {
+			escrowsResults[r2] += 1
+		}
+
+		if r1 == 0 || r2 == 0 {
+			items_test = append(items_test, items[i])
+		}
+    }
+
+	request := func(next []byte, offset, limit uint64, total bool) *types.QueryEscrowsByAddressRequest {
+		return &types.QueryEscrowsByAddressRequest{
+			Address: addresses[0],
 			Pagination: &query.PageRequest{
 				Key:        next,
 				Offset:     offset,
@@ -98,12 +119,13 @@ func TestEscrowQueryByAddress(t *testing.T) {
 	}
 	t.Run("ByOffset", func(t *testing.T) {
 		step := 2
-		for i := 0; i < len(msgs); i += step {
-			resp, err := keeper.EscrowAll(wctx, request(nil, uint64(i), uint64(step), false))
+		for i := 0; i < len(items); i += step {
+			resp, err := keeper.EscrowsByAddress(wctx, request(nil, uint64(i), uint64(step), false))
 			require.NoError(t, err)
-			require.LessOrEqual(t, len(resp.Escrow), step)
+			count := len(resp.Escrow)
+			require.LessOrEqual(t, count, step)
 			require.Subset(t,
-				nullify.Fill(msgs),
+				nullify.Fill(items_test),
 				nullify.Fill(resp.Escrow),
 			)
 		}
@@ -111,23 +133,23 @@ func TestEscrowQueryByAddress(t *testing.T) {
 	t.Run("ByKey", func(t *testing.T) {
 		step := 2
 		var next []byte
-		for i := 0; i < len(msgs); i += step {
-			resp, err := keeper.EscrowAll(wctx, request(next, 0, uint64(step), false))
+		for i := 0; i < len(items); i += step {
+			resp, err := keeper.EscrowsByAddress(wctx, request(next, 0, uint64(step), false))
 			require.NoError(t, err)
 			require.LessOrEqual(t, len(resp.Escrow), step)
 			require.Subset(t,
-				nullify.Fill(msgs),
+				nullify.Fill(items_test),
 				nullify.Fill(resp.Escrow),
 			)
 			next = resp.Pagination.NextKey
 		}
 	})
 	t.Run("Total", func(t *testing.T) {
-		resp, err := keeper.EscrowAll(wctx, request(nil, 0, 0, true))
+		resp, err := keeper.EscrowsByAddress(wctx, request(nil, 0, 0, true))
 		require.NoError(t, err)
-		require.Equal(t, len(msgs), int(resp.Pagination.Total))
+		require.Equal(t, len(items), int(resp.Pagination.Total))
 		require.ElementsMatch(t,
-			nullify.Fill(msgs),
+			nullify.Fill(items_test),
 			nullify.Fill(resp.Escrow),
 		)
 	})
@@ -135,4 +157,4 @@ func TestEscrowQueryByAddress(t *testing.T) {
 		_, err := keeper.EscrowAll(wctx, nil)
 		require.ErrorIs(t, err, status.Error(codes.InvalidArgument, "invalid request"))
 	})
-}*/
+}
