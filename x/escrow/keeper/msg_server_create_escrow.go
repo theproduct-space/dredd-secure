729de6a96fe6be00e836d3d889c88d1cb44d9bdc
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2023-08-23 12:21:52 -0400

    Unit testing for tips functionality

--- x/escrow/keeper/msg_server_create_escrow.go
+++ x/escrow/keeper/msg_server_create_escrow.go
@@ -33,17 +33,14 @@ func (k msgServer) CreateEscrow(goCtx context.Context, msg *types.MsgCreateEscro
 	}
 
 	// Transfer the initiator's coins from their account to the escrow module
-	errSendCoins := k.bank.SendCoinsFromAccountToModule(ctx, initiator, types.ModuleName, escrow.InitiatorCoins)
-	if errSendCoins != nil {
-		return nil, errors.Wrapf(errSendCoins, types.ErrInitiatorCannotPay.Error())
+	sendingCoins := escrow.InitiatorCoins
+	if escrow.Tips != nil {
+		sendingCoins = []sdk.Coin{escrow.InitiatorCoins[0], escrow.Tips[0]}
 	}
 
-	if escrow.Tips != nil {
-			// Transfer the initiator's coins from their account to the escrow module
-		errSendCoinsTips := k.bank.SendCoinsFromAccountToModule(ctx, initiator, types.ModuleName, escrow.Tips)
-		if errSendCoinsTips != nil {
-			return nil, errors.Wrapf(errSendCoinsTips, types.ErrInitiatorCannotPay.Error())
-		}
+	errSendCoins := k.bank.SendCoinsFromAccountToModule(ctx, initiator, types.ModuleName, sendingCoins)
+	if errSendCoins != nil {
+		return nil, errors.Wrapf(errSendCoins, types.ErrInitiatorCannotPay.Error())
 	}
 
 	// Append the newly created escrow to the store
