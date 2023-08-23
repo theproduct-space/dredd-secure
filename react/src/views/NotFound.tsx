4c79dfca7d86957e5aee4c108201745181e732df
Xzandro <xzanvas@gmail.com>
2023-08-23 16:26:59 -0400

    added missing file

---
+++ react/src/views/NotFound.tsx
@@ -0,0 +1,16 @@
+import Typography from "~baseComponents/Typography";
+
+const NotFound = (props: NotFoundProps) => {
+  return (
+    <div className="h-screen w-100 bg-black bg-no-repeat bg-center bg-cover flex items-center justify-center">
+      <Typography
+        variant="h1"
+        className="text-white-1000 font-revalia text-orange"
+      >
+        404 Not Found
+      </Typography>
+    </div>
+  );
+};
+
+export default NotFound;
