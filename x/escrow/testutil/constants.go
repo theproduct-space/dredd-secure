8a9bad861d069ebfeedb76b17dcca1e7abbe8a87
jayouimet <jayouimet@hotmail.com>
2023-09-11 16:40:46 -0400

    added test based on an error on testnet

--- x/escrow/testutil/constants.go
+++ x/escrow/testutil/constants.go
@@ -3,4 +3,5 @@ package testutil
 const (
 	Alice = "cosmos15dpeq2jnm4575yz86j2dfll2qhv05qtpy2w4qy"
 	Bob   = "cosmos1hq3y3y9w8k2dqr0ywq39ted4zm6ljal69r3v6k"
+	TipAccount = "cosmos15dpeq2jnm4575yz86j2dfll2qhv05qtpy2w4qy"
 )
